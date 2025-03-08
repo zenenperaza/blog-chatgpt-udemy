6ee8a28c113a56cc566e352b6b6deded8db71ccf
zenenperaza <peraza@outlook.com>
2025-03-07 21:55:04 -0400

    7-3-25

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'role' => $this->faker->randomElement(['admin', 'editor', 'user']),
+            'role' => $this->faker->randomElement(['admin', 'author', 'reader']),
             'remember_token' => Str::random(10),
             'created_at' => now(),
             'updated_at' => now(),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+               \App\Models\User::factory()->create(['name' => 'Admin User', 'email' => 'admin@admin.com']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+               \App\Models\User::factory(4)->create();
+
+               $categories = \App\Models\Category::factory(5)->create();
+
+               $tags = \App\Models\Tag::factory(5)->create();
+               
+               $post = \App\Models\Post::factory(50)->create();
+
+               $post->each(function($post) use ($categories, $tags){
+
+                    $post->categories()->attach(
+                        $categories->random(rand(1, 3))->pluck('id')->toArray()
+                    );
+
+                    $post->tags()->attach(
+                        $tags->random(rand(1, 5))->pluck('id')->toArray()
+                    );
+
+
+               });
     }
 }
